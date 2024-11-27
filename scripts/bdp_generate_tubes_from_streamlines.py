f46dcf317d24d43b5806cd597c78601f1faf9513
frheault <francois.m.rheault@usherbrooke.ca>
2024-11-27 11:04:22 -0500

    Coloring tubes

--- scripts/bdp_generate_tubes_from_streamlines.py
+++ scripts/bdp_generate_tubes_from_streamlines.py
@@ -17,20 +17,20 @@ Use the following script with your streamlines (as TRK/TCK):
 Then call this script with the scaled TRK/TCK as input:
     bdp_generate_tubes_from_streamlines.py - This will generate tubes.
 """
-
-
 import argparse
 import os
 
-from dipy.io.streamline import load_tractogram, StatefulTractogram
-from dipy.tracking.streamlinespeed import compress_streamlines
+from dipy.io.streamline import load_tractogram
 import numpy as np
+from scipy.spatial import KDTree
+from scilpy.tractograms.streamline_operations import compress_sft
 import vtk
 
 from bradiphopy.bradipho_helper import BraDiPhoHelper3D
+from bradiphopy.fury import lines_to_vtk_polydata
 from bradiphopy.io import save_polydata
 
-from bradiphopy.fury import lines_to_vtk_polydata
+NB_SIDE = 6
 
 
 def _build_arg_parser():
@@ -42,18 +42,22 @@ def _build_arg_parser():
                    help='Radius of the tubes (mm).')
     p.add_argument('out_filename',
                    help='Output bundle filename (.ply).')
-    p.add_argument('--color', nargs=3, type=int, default=(255, 255, 255),
-                   help='Color as RGB (0-255).')
+
+    color = p.add_mutually_exclusive_group()
+    color.add_argument('--color', nargs=3, type=int, default=(255, 255, 255),
+                       help='Color as RGB (0-255).')
+    color.add_argument('--keep_color', action='store_true',
+                       help='Keep the color of the input streamlines.')
+
     p.add_argument('--ascii', action='store_true',
                    help='Save the file with data as ASCII '
                         '(instead of binary).')
 
     p.add_argument('--scaling', type=float, default=1.0,
                    help='Scaling factor to apply to the streamlines [%(default)s].')
-    p.add_argument('--tol_error', type=float, default=0.0001,
+    p.add_argument('--tol_error', type=float, const=0.1, nargs='?',
                    help='Tolerance error for the compression of the streamlines.'
-                        'Around 0.1mm is NIFTI space.\n'
-                        'Default is 0.0001mm, for photogrammetry space.')
+                        'Around 0.1mm in RASMM space.')
     p.add_argument('--reference', default='same',
                    help='Reference image for the output file.')
 
@@ -69,69 +73,60 @@ def create_tubes(streamlines, radius):
     tubeFilter = vtk.vtkTubeFilter()
     tubeFilter.SetInputData(polydata)
     tubeFilter.SetRadius(float(radius))
-    tubeFilter.SetNumberOfSides(6)
+    tubeFilter.SetNumberOfSides(NB_SIDE)
     tubeFilter.CappingOn()
     tubeFilter.Update()
 
     return tubeFilter.GetOutput()
 
 
-def compress_sft(sft, tol_error=0.2):
-    """ """
-    # Go to world space
-    orig_space = sft.space
-    sft.to_rasmm()
-
-    compressed_streamlines = compress_streamlines(sft.streamlines,
-                                                  tol_error=tol_error)
-
-    compressed_sft = StatefulTractogram.from_sft(
-        compressed_streamlines, sft,
-        data_per_streamline=sft.data_per_streamline)
-
-    # Return to original space
-    compressed_sft.to_space(orig_space)
-
-    return compressed_sft
-
-
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
+    if args.keep_color and args.tol_error:
+        raise ValueError('Cannot keep color and have compressioj.')
+
     if os.path.isfile(args.out_filename) and not args.overwrite:
         raise IOError(
-            '{} already exists, use -f to overwrite.'.format(args.out_filename))
+            f'{args.out_filename} already exists, use -f to overwrite.')
 
     _, ext = os.path.splitext(args.out_filename)
     if ext != '.ply':
         raise ValueError('Output file must be a .ply file to support color.')
 
+    sft = load_tractogram(args.in_bundle, args.reference)
+
+    if args.tol_error:
+        sft = compress_sft(sft, tol_error=args.tol_error)
+
     aff = np.eye(3) * args.scaling
     if ext in ['.trk', '.tck']:
         aff[0, 0] *= -1
         aff[1, 1] *= -1
-
-    sft = compress_sft(load_tractogram(args.in_bundle, args.reference),
-                       tol_error=args.tol_error)
     sft.streamlines._data = np.dot(sft.streamlines._data, aff)
+
     polydata = create_tubes(sft.streamlines, args.radius)
     obj_w_strip = BraDiPhoHelper3D(polydata)
 
-    obj_w_polys = BraDiPhoHelper3D.generate_bdp_obj(
-        obj_w_strip.get_polydata_vertices(),
-        obj_w_strip.get_polydata_triangles())
-
     # Flip triangles
     normal_generator = vtk.vtkPolyDataNormals()
-    normal_generator.SetInputData(obj_w_polys.get_polydata())
+    normal_generator.SetInputData(obj_w_strip.get_polydata())
     normal_generator.ComputeCellNormalsOff()
     normal_generator.ConsistencyOn()
     normal_generator.Update()
 
     obj_out = BraDiPhoHelper3D(normal_generator.GetOutput())
-    color_arr = np.zeros((len(obj_out), 3))
-    color_arr[:] = args.color
+
+    if args.keep_color and 'color' in sft.data_per_point:
+        kdtree = KDTree(sft.streamlines._data)
+        _, ind = kdtree.query(obj_out.get_polydata_vertices())
+        color_arr = sft.data_per_point['color']._data[ind]
+
+    else:
+        color_arr = np.zeros((len(obj_out), 3))
+        color_arr[:] = args.color
+
     obj_out.set_scalar(color_arr, 'RGB', dtype='uint8')
 
     save_polydata(obj_out.get_polydata(), args.out_filename, ascii=args.ascii)
